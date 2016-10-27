502e4a5b09ec3c5720abce6a4de5501940343448
Ross Cohen <rjc55@njit.edu>
2016-10-27 10:37:54 -0400

    Added keyword to skip server query

--- README.md
+++ README.md
@@ -2,10 +2,18 @@
 This will check for cdfs on your computer. If they don't exist, or there's an updated version on the server, it will download from the server. It will then load the cdfs using spacepy.pycdf, and place them into a dictionary.
 
 ##Usage:
-getCDFs(getCDFs(datetime, craft, species, PH='LEHT', EMF='1sec-sm', all=True, TOFxE=False, TOFxPH=False, HOPE=False, EMFISIS=False):
+getCDFs(getCDFs(datetime, string, string, **kw):
 
-datetime is a Python datetime object, craft is a character: 'A' or 'B', species is a character: 'H', 'He', or 'O', PH is a string describing which Time of Flight by Pulse Height product you want: 'LEHT'(Low Energy, High Time resolution) or 'HELT'(High Energy, Low Time resolution), EMF is a string describing which EMFISIS product you want: ('1sec', '4sec', or 'hires')+'-'+('gei', 'geo', 'gse', 'gsm', or 'sm'). The last five keywords describe which data product you want, all is default, but if you set any one or more of them to True, then you will only get what you set to True.
+    date: Python datetime object containing the date that you want cdfs for.
+    craft: character: 'A' or 'B'.
+    species: character: 'H', 'He', or 'O'.
+    PH: string describing which Time of Flight by Pulse Height product you want: 'LEHT'(Low Energy, High Time resolution) or 'HELT'(High Energy, Low Time resolution).
+    EMF: string describing which EMFISIS product you want: ('1sec', '4sec', or 'hires')+'-'+('gei', 'geo', 'gse', 'gsm', or 'sm').
+    check: When True, it will query the server for updated versions, if it is False, then the server will not be queried at all. If you don't have the file on your computer, it will not be downloaded. Set this keyword to False if you do not have internet access.
+    The last five keywords describe which data product you want, all is default, but if you set any one or more of them to True, then you will only get what you set to True.
 
+    Returns: Python dictionary containing spacepy cdf objects. Keys are 'TOFxE', 'TOFxPH', 'HOPE', and 'EMFISIS'
+	
 changeRoot()
 
-On the first run, you will be asked to set a root directory where your files will be stored. This function will allow you to change this.
+	On the first run, you will be asked to set a root directory where your files will be stored. This function will allow you to change this.
--- getCDFs.py
+++ getCDFs.py
@@ -1,10 +1,23 @@
 
 # coding: utf-8
 
-# In[28]:
+# In[3]:
 
-def getCDFs(datetime, craft, species, PH='LEHT', EMF='1sec-sm',
+def getCDFs(date, craft, species, PH='LEHT', EMF='1sec-sm', check=True,
             all=True, TOFxE=False, TOFxPH=False, HOPE=False, EMFISIS=False):
+    '''
+    getCDFs(getCDFs(datetime, string, string, **kw):
+
+    date: Python datetime object containing the date that you want cdfs for.
+    craft: character: 'A' or 'B'.
+    species: character: 'H', 'He', or 'O'.
+    PH: string describing which Time of Flight by Pulse Height product you want: 'LEHT'(Low Energy, High Time resolution) or 'HELT'(High Energy, Low Time resolution).
+    EMF: string describing which EMFISIS product you want: ('1sec', '4sec', or 'hires')+'-'+('gei', 'geo', 'gse', 'gsm', or 'sm').
+    check: When True, it will query the server for updated versions, if it is False, then the server will not be queried at all. If you don't have the file on your computer, it will not be downloaded. Set this keyword to False if you do not have internet access.
+    The last five keywords describe which data product you want, all is default, but if you set any one or more of them to True, then you will only get what you set to True.
+
+    Returns: Python dictionary containing spacepy cdf objects. Keys are 'TOFxE', 'TOFxPH', 'HOPE', and 'EMFISIS'
+    '''
     
     cdfs = {}
     
@@ -55,8 +68,16 @@ def getCDFs(datetime, craft, species, PH='LEHT', EMF='1sec-sm',
         EMFISIS = True
         
     def getTOFxE():
-        url = 'http://rbspice'+craft.lower()+'.ftecs.com/Level_3PAP/TOFxE'+species+'/'+datetime.strftime('%Y')+'/'
+        url = 'http://rbspice'+craft.lower()+'.ftecs.com/Level_3PAP/TOFxE'+species+'/'+date.strftime('%Y')+'/'
         destination = root+sep+craft+sep+'TOFxE'+species+sep
+        if not check:
+            file = filter(listdir(destination), '*'+date.strftime('%Y%m%d')+'*')
+            if not file:
+                print('No TOFxE file')
+                return
+            else:
+                print('Loading TOFxE...')
+                return cdf.CDF(destination+file[0])
         try:
             stat(destination)
         except:
@@ -68,7 +89,7 @@ def getCDFs(datetime, craft, species, PH='LEHT', EMF='1sec-sm',
         page = request.text
         soup = BeautifulSoup(page, 'html.parser')
         files = [node.get('href') for node in soup.find_all('a') if node.get('href').endswith('.cdf')]
-        fileList = filter(files, '*'+datetime.strftime('%Y%m%d')+'*')
+        fileList = filter(files, '*'+date.strftime('%Y%m%d')+'*')
         if not fileList:
             print('Month or day does not exist for TOFxE'+species+' '+craft)
             return
@@ -96,8 +117,16 @@ def getCDFs(datetime, craft, species, PH='LEHT', EMF='1sec-sm',
             return newCDF
 
     def getTOFxPH():
-        url = 'http://rbspice'+craft.lower()+'.ftecs.com/Level_3PAP/TOFxPH'+species+PH+'/'+datetime.strftime('%Y')+'/'
+        url = 'http://rbspice'+craft.lower()+'.ftecs.com/Level_3PAP/TOFxPH'+species+PH+'/'+date.strftime('%Y')+'/'
         destination = root+sep+craft+sep+'TOFxPH'+species+PH+sep
+        if not check:
+            file = filter(listdir(destination), '*'+date.strftime('%Y%m%d')+'*')
+            if not file:
+                print('No TOFxPH file')
+                return
+            else:
+                print('Loading TOFxPH...')
+                return cdf.CDF(destination+file[0])
         try:
             stat(destination)
         except:
@@ -109,7 +138,7 @@ def getCDFs(datetime, craft, species, PH='LEHT', EMF='1sec-sm',
         page = request.text
         soup = BeautifulSoup(page, 'html.parser')
         files = [node.get('href') for node in soup.find_all('a') if node.get('href').endswith('.cdf')]
-        fileList = filter(files, '*'+datetime.strftime('%Y%m%d')+'*')
+        fileList = filter(files, '*'+date.strftime('%Y%m%d')+'*')
         if not fileList:
             print('Month or day does not exist for TOFxPH'+species+PH+' '+craft)
             return
@@ -139,6 +168,14 @@ def getCDFs(datetime, craft, species, PH='LEHT', EMF='1sec-sm',
     def getHOPE():
         url = 'https://rbsp-ect.lanl.gov/data_pub/rbsp'+craft.lower()+'/hope/level3/PA/'
         destination = root+sep+craft+sep+'HOPE'+sep
+        if not check:
+            file = filter(listdir(destination), '*'+date.strftime('%Y%m%d')+'*')
+            if not file:
+                print('No HOPE file')
+                return
+            else:
+                print('Loading HOPE...')
+                return cdf.CDF(destination+file[0])
         try:
             stat(destination)
         except:
@@ -146,7 +183,7 @@ def getCDFs(datetime, craft, species, PH='LEHT', EMF='1sec-sm',
         page = requests.get(url).text
         soup = BeautifulSoup(page, 'html.parser')
         files = [node.get('href') for node in soup.find_all('a') if node.get('href').endswith('.cdf')]
-        fileList = filter(files, '*'+datetime.strftime('%Y%m%d')+'*')
+        fileList = filter(files, '*'+date.strftime('%Y%m%d')+'*')
         if not fileList:
             print('Date does not exist for HOPE '+craft)
             return
@@ -174,8 +211,16 @@ def getCDFs(datetime, craft, species, PH='LEHT', EMF='1sec-sm',
             return newCDF
         
     def getEMFISIS():
-        url = 'http://emfisis.physics.uiowa.edu/Flight/RBSP-'+craft+'/L3/'+datetime.strftime('%Y/%m/%d/')
+        url = 'http://emfisis.physics.uiowa.edu/Flight/RBSP-'+craft+'/L3/'+date.strftime('%Y/%m/%d/')
         destination = root+sep+craft+sep+'EMFISIS'+sep
+        if not check:
+            file = filter(listdir(destination), '*'+date.strftime('%Y%m%d')+'*')
+            if not file:
+                print('No EMFISIS file')
+                return
+            else:
+                print('Loading EMFISIS...')
+                return cdf.CDF(destination+file[0])
         try:
             stat(destination)
         except:
@@ -190,7 +235,7 @@ def getCDFs(datetime, craft, species, PH='LEHT', EMF='1sec-sm',
         page = request.text
         soup = BeautifulSoup(page, 'html.parser')
         files = [node.get('href') for node in soup.find_all('a') if node.get('href').endswith('.cdf')]
-        file = url + filter(files, '*'+EMF+'*'+datetime.strftime('%Y%m%d')+'*')[-1]
+        file = url + filter(files, '*'+EMF+'*'+date.strftime('%Y%m%d')+'*')[-1]
         fname = file[file.rfind('/')+1:]
         fnameNoVer = fname[:fname.rfind('v')+1]
         prevFile = filter(listdir(destination), fnameNoVer+'*')
@@ -244,6 +289,9 @@ def getCDFs(datetime, craft, species, PH='LEHT', EMF='1sec-sm',
     return cdfs
 
 def changeRoot():
+    '''
+    Running this will query you for a new root folder, which will be stored in your config file.
+    '''
     from configparser import ConfigParser
     from os.path import normpath
     root = normpath(input('Please input a root directory for your cdf files:'))
@@ -252,4 +300,9 @@ def changeRoot():
     config['Directories'] = {'root':root}
     with open('getCDFsConfig.ini', 'w') as configfile:
         config.write(configfile)
+
+
+# In[ ]:
+
+
 
